1a4f30e2fd2efabd43493949fc3eb07356059f25
Ademar Júnior <ademar.barbosa@gmail.com>
2018-03-12 09:50:10 -0300

    Corrigindo problema nas entidades de domínio

--- src/main/java/br/com/coretecnologia/cursomc/domain/Pagamento.java
+++ src/main/java/br/com/coretecnologia/cursomc/domain/Pagamento.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import br.com.coretecnologia.cursomc.domain.enums.EstadoPagamento;
 
@@ -24,7 +24,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 	
-	@JsonManagedReference
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
