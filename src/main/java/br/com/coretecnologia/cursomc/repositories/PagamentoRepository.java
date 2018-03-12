160a2e8a93d0b89d8e8cbc0a66c4a107eafa0001
ademarbarbosa <ademar.barbosa@gmail.com>
2018-03-11 22:16:17 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/br/com/coretecnologia/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,13 @@
+package br.com.coretecnologia.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.coretecnologia.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+	
+	
+}
