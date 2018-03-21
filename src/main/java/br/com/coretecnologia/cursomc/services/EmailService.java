eaa4461af542cd2c429cacd404997b4d726ab95d
Ademar Júnior <ademar.barbosa@gmail.com>
2018-03-21 13:41:29 -0300

    Email HTML

--- src/main/java/br/com/coretecnologia/cursomc/services/EmailService.java
+++ src/main/java/br/com/coretecnologia/cursomc/services/EmailService.java
@@ -1,5 +1,6 @@
 package br.com.coretecnologia.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
 import org.springframework.mail.SimpleMailMessage;
 import br.com.coretecnologia.cursomc.domain.Pedido;
 
@@ -8,5 +9,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
