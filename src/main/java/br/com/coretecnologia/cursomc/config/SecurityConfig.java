fbea677af0f6982d694d451e324aab59069d9827
Ademar Júnior <ademar.barbosa@gmail.com>
2018-03-23 15:03:25 -0300

    Implementando autorização

--- src/main/java/br/com/coretecnologia/cursomc/config/SecurityConfig.java
+++ src/main/java/br/com/coretecnologia/cursomc/config/SecurityConfig.java
@@ -18,8 +18,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import br.com.coretecnologia.cursomc.security.JWTUtil;
 import br.com.coretecnologia.cursomc.security.JWTAuthenticationFilter;
+import br.com.coretecnologia.cursomc.security.JWTAuthorizationFilter;
+import br.com.coretecnologia.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
